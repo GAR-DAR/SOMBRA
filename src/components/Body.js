8f57af50e5dd41684c40fb2f11267c6701011fda
Petqka <112760242+Petqka@users.noreply.github.com>
2024-11-20 20:59:26 +0200

    add (#6)
    * add
    
    * addd
    
    ---------
    
    Co-authored-by: GribanovIvan <40523124+GribanovIvan@users.noreply.github.com>

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,6 +10,13 @@ const Body = () => {
           <p style={styles.position}>QA</p>
         </div>
       </div>
+      <div style={styles.card}>
+        <img src={"/petro.jpeg"} alt="Petro Broda" style={styles.photo} />
+        <div style={styles.textContainer}>
+          <h2 style={styles.name}>Petro Broda</h2>
+          <p style={styles.position}>Kotlin engineer</p>
+        </div>
+      </div>
           <div style={styles.card}>
               <img src={"/bohdan_pic.jpg"} alt="Bohdan Bilynskiy" style={styles.photo} />
               <div style={styles.textContainer}>
